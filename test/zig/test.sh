8fcaa9d208c0722ec65fd600c09b57ee1fa809bc
Mike Wallio <michael.wallio@outlook.com>
2025-10-18 23:33:17 +0000

    Add zig feature

---
+++ test/zig/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This test file will be executed against the 'default_version' scenario
+# that includes the 'zig' feature without a version specified.
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "zig is installed" bash -c "zig version"
+
+# Report result
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
